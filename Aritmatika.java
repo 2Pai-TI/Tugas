f3592c13609f8290b1be048e5dae49d2398e8f1a
2Pai-TI <80019360+2Pai-TI@users.noreply.github.com>
2021-03-04 11:12:49 +0700

    Aritmatika penjumlahan 2 Variable dengan input

--- Aritmatika.java
+++ Aritmatika.java
@@ -3,9 +3,18 @@ import java.util.Scanner;
 public class Aritmatika{
   
   public static void main(String []args) {
-    int a=1, b=2, hasil;
+    int a, b, hasil;
     
-    hasil = a / b;
+    
+    Scanner test = new Scanner(System.in);
+    
+    System.out.print("Masukkan nilai a: ")
+    a = test.nextInt();
+    
+    System.out.print("Masukkan nilai b: ")
+    b = test.nextInt();
+    
+    hasil = a + b;
     System.out.println("Hasil =" +hasil);
     
   }
