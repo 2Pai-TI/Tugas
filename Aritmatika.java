ce05608857caeba5e6ec08879203c8693705edb9
2Pai-TI <80019360+2Pai-TI@users.noreply.github.com>
2021-03-04 11:06:39 +0700

    Create Aritmatika.java

---
+++ Aritmatika.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Aritmatika{
+  
+  public static void main(String []args) {
+    int a=1, b=2, hasil;
+    
+    hasil = a + b;
+    System.out.println("Hasil =" +hasil);
+    
+  }
+}
